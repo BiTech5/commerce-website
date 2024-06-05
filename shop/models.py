1a3a1bfeb7f742d6216890f70b4e168a5d30e65b
BiTech5 <bikikc7@gmail.com>
2024-06-05 13:15:54 +0545

    Adding the category

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    title=models.CharField(max_length=60)
+    brand=models.CharField(max_length=60)
+    short_desc=models.CharField(max_length=300)
+    long_desc=models.TextField()
+    org_price=models.IntegerField()
+    dis_price=models.IntegerField()
+    img=models.ImageField(upload_to='image/')
+
